59583deac97bbc7e4bdd91606add3cd7d7c2c860
GyanendraMohan <gmpatel152@gmail.com>
2022-10-18 22:10:57 +0530

    hooked up with map search bar

--- src/features/restaurants/map/screens/map.screen.js
+++ src/features/restaurants/map/screens/map.screen.js
@@ -1,6 +1,18 @@
 import React from "react";
-
 import { useContext, useState, useEffect } from "react";
 import MapView from "react-native-maps";
 
-export const MapScreen = () => <MapView style={{ height: "100%" }} />;
+import styled from "styled-components/native";
+import { Search } from "../components/search.component";
+
+const Map = styled(MapView)`
+  height: 100%;
+  width: 100%;
+`;
+
+export const MapScreen = () => (
+  <>
+    <Search />
+    <Map />
+  </>
+);
