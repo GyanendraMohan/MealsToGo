50b59ba14e268ad73558e2aa1a9c8c76675ac931
GyanendraMohan <gmpatel152@gmail.com>
2022-10-18 12:30:47 +0530

    Added data and scrollable view

--- src/features/restaurants/screens/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import { List } from "react-native-paper";
-
+import { ScrollView } from "react-native";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurants-info-card.component";
 
@@ -15,30 +15,51 @@ export const RestaurantDetailScreen = ({ route }) => {
   return (
     <SafeArea>
       <RestaurantInfoCard restaurant={restaurant} />
-      <List.Accordion
-        title="Breakfast"
-        left={(props) => <List.Icon {...props} icon="bread-slice" />}
-        expanded={breakfastExpanded}
-        onPress={() => setBreakfastExpanded(!breakfastExpanded)}
-      ></List.Accordion>
-      <List.Accordion
-        title="Lunch"
-        left={(props) => <List.Icon {...props} icon="hamburger" />}
-        expanded={lunchExpanded}
-        onPress={() => setLunchExpanded(!lunchExpanded)}
-      ></List.Accordion>
-      <List.Accordion
-        title="Dinner"
-        left={(props) => <List.Icon {...props} icon="food-variant" />}
-        expanded={dinnerExpanded}
-        onPress={() => setDinnerExpanded(!dinnerExpanded)}
-      ></List.Accordion>
-      <List.Accordion
-        title="Drinks"
-        left={(props) => <List.Icon {...props} icon="cup" />}
-        expanded={drinksExpanded}
-        onPress={() => setDrinksExpanded(!drinksExpanded)}
-      ></List.Accordion>
+      <ScrollView>
+        <List.Accordion
+          title="Breakfast"
+          left={(props) => <List.Icon {...props} icon="bread-slice" />}
+          expanded={breakfastExpanded}
+          onPress={() => setBreakfastExpanded(!breakfastExpanded)}
+        >
+          <List.Item title="Sandwich" />
+          <List.Item title="Oats" />
+          <List.Item title="Sprouts" />
+        </List.Accordion>
+        <List.Accordion
+          title="Lunch"
+          left={(props) => <List.Icon {...props} icon="hamburger" />}
+          expanded={lunchExpanded}
+          onPress={() => setLunchExpanded(!lunchExpanded)}
+        >
+          <List.Item title="Dal" />
+          <List.Item title="Rice" />
+          <List.Item title="Paneer" />
+        </List.Accordion>
+        <List.Accordion
+          title="Dinner"
+          left={(props) => <List.Icon {...props} icon="food-variant" />}
+          expanded={dinnerExpanded}
+          onPress={() => setDinnerExpanded(!dinnerExpanded)}
+        >
+          <List.Item title="Dal $ Rice" />
+          <List.Item title="Vegetables " />
+          <List.Item title="Mushroom soup" />
+        </List.Accordion>
+        <List.Accordion
+          title="Drinks"
+          left={(props) => <List.Icon {...props} icon="cup" />}
+          expanded={drinksExpanded}
+          onPress={() => setDrinksExpanded(!drinksExpanded)}
+        >
+          <List.Item title="Keventers Milk Shakes" />
+          <List.Item title="Lemmon drink" />
+          <List.Item title="Blue Lagoon" />
+          <List.Item title="Coffee" />
+          <List.Item title="Tea" />
+          <List.Item title="Modelo" />
+        </List.Accordion>
+      </ScrollView>
     </SafeArea>
   );
 };
