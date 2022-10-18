1a5defa8ba3c4923427fa3e1067731f70d366c4c
GyanendraMohan <gmpatel152@gmail.com>
2022-10-18 11:29:52 +0530

    implemented detail screen popup

---
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { RestaurantInfoCard } from "../components/restaurants-info-card.component";
+
+export const RestaurantDetailScreen = ({ route }) => {
+  const { restaurant } = route.params;
+  return (
+    <SafeArea>
+      <RestaurantInfoCard restaurant={restaurant} />
+    </SafeArea>
+  );
+};
--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -6,7 +6,8 @@ import {
 } from "@react-navigation/stack";
 
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
-import { Text } from "react-native";
+
+import { RestaurantDetailScreen } from "../../features/restaurants/screens/restaurant-detail.screen";
 
 const RestaurantStack = createStackNavigator();
 
@@ -22,7 +23,7 @@ export const RestaurantsNavigator = () => {
       />
       <RestaurantStack.Screen
         name="RestaurantDetail"
-        component={() => <Text>Restaurants Detail</Text>}
+        component={RestaurantDetailScreen}
       />
     </RestaurantStack.Navigator>
   );
