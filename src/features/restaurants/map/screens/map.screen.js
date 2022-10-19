216a4c5f349a5b6bfd443d4df938e20c40b0fcd4
GyanendraMohan <gmpatel152@gmail.com>
2022-10-19 10:41:44 +0530

    completed location search on map

--- src/features/restaurants/map/screens/map.screen.js
+++ src/features/restaurants/map/screens/map.screen.js
@@ -3,16 +3,45 @@ import { useContext, useState, useEffect } from "react";
 import MapView from "react-native-maps";
 
 import styled from "styled-components/native";
+import { LocationContext } from "../../../../services/location/location.context";
 import { Search } from "../components/search.component";
+import { RestaurantsContext } from "../../../../services/restaurants/restaurants.context";
 
 const Map = styled(MapView)`
   height: 100%;
   width: 100%;
 `;
 
-export const MapScreen = () => (
-  <>
-    <Search />
-    <Map />
-  </>
-);
+export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const [latDelta, setLatDelta] = useState(0);
+  const { viewport, lat, lng } = location;
+
+  console.log(viewport.northeast);
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
+  return (
+    <>
+      <Search />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return null;
+        })}
+      </Map>
+    </>
+  );
+};
